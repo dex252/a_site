4f6ccb8c7c963fa075df4fad704a6299febf933c
Juen <korobkaboox@gmail.com>
2019-05-07 12:28:57 +0300

    structure creation

---
+++ сайт/Assets/functions.js
@@ -0,0 +1,12 @@
+function showModal() {
+    var darkLayer = document.createElement('div'); // слой затемнения
+    darkLayer.id = 'shadow'; // id чтобы подхватить стиль
+    document.body.appendChild(darkLayer); // включаем затемнение
+ 
+    var modalWin = document.getElementById('moda'); // находим наше "окно"
+    modalWin.style.display = 'block'; // "включаем" его
+    darkLayer.onclick = function () {  // при клике на слой затемнения все исчезнет
+    darkLayer.parentNode.removeChild(darkLayer); // удаляем затемнение
+    modalWin.style.display = 'none'; // делаем окно невидимым
+    return false;
+};}
