0d8dcc3ca3f7a1b5ce71f9aa130ab6db0c14fd24
Slava Solodkov <dex252@bk.ru>
2019-05-15 21:38:53 +1000

    Add searchPanel

--- index.php
+++ index.php
@@ -19,84 +19,18 @@
 <body>
 
     <header>
-		<div id="logo" class="Header">
-				<img src="./img/header/background.png" class="HeaderImage">
-				<div id="register">
-					<a href="./index.html" type="text/css" class="TextMenuStyle">
-						<div class="ButtonRegistration" >Регистарция</div>
-					</a>
-				</div>
-				<div id="enter">
-					<a href="./index.html" type="text/css" class="TextMenuStyle">
-						<div class="ButtonEnter"> Вход</div>
-					</a>
-				</div>
-		</div> 
-		
-		<div id="menu">
-			<div id="searchMenu">
-				<span class="SearchingClass" onclick="StartSearch()">
-					<div id ="searchSymbol"></div>
-				</span>
-			
-				<div id="searchText">
-					<p><input class="searchTextInput" 
-								style="font-size:25px; 
-									font-family: Comic Sans MS, Comic Sans, cursive; 
-									color: deepskyblue;"
-								type="search" 
-								name="searhInput" 
-								placeholder="Найти аниме по названию"> 
-					</p>
-				</div>
-			</div>
-			
-			<div id="buttonMainMenuHelpType">
-				<a href="./index.html" type="text/css" class="TextMenuStyle">
-					<div class="ButtonMenuWhite" >Помощь</div>
-				</a>
-			</div>
-			
-			<div id="buttonRandomMenuType">
-				<a href="./index.html" type="text/css" class="TextMenuStyle">
-					<div class="ButtonMenuWhite">Случайное
-						<font size="3"><span style='padding-left:55px;'> </span>аниме</font>
-					</div>
-				</a>
-			</div>
-			
-			<div id="buttonMainMenuTop100" class="buttonMainMenu">
-				<a href="./index.html" type="text/css" class="TextMenuStyle">
-					<div class="ButtonMenuWhite">Топ-100
-						<font size="3"><span style='padding-left:30px;'></span>аниме</font>
-					</div>
-				</a>
-			</div>
-			
-			<div id="buttonMainMenuGeneral" class="buttonMainMenu">
-				<a href="./index.html" type="text/css" class="TextMenuStyle">
-					<div class="ButtonMenuWhite">Главная</div>
-				</a>
-			</div>
-
-		</div>
-		
-		<div id ="animeBlocks">
-			<?php
-				for($i=1; $i<=12; $i++)
-				{
-					echo ('
-						<div id="block'.$i.'" class="animeBlock">
-							
-						</div>
-						');
-				}
-			?>
-
-		</div>
+		<?php include "./pages/header.php"; ?>
 	</header>
-
-
+	
+	<div id="foundation">
+		<div id ="animeBlocks">
+			<?php include "./pages/theMain/animeBlocks.php"; ?>
+		</div><div id="searchContainer">
+	
+		</div>
+		
+	</div>	
+	
 </body>
 
 </html>
---
+++ pages/theMain/animeBlocks.php
@@ -0,0 +1,10 @@
+<?php
+for($i=1; $i<=12; $i++)
+				{
+					echo ('
+						<div id="block'.$i.'" class="animeBlock">
+							
+						</div>
+						');
+				}
+?>
