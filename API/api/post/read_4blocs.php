773c1cbff1d793fabf3ed4f0bcaec28bfe0cd134
Editscode <38367082+Editscode@users.noreply.github.com>
2019-06-08 16:03:10 +1000

    fix read4

--- API/api/post/read_4blocs.php
+++ API/api/post/read_4blocs.php
@@ -5,7 +5,12 @@
 
   include_once '../../config/Database.php';
   include_once '../../models/Post.php';
-
+  $posts_arr2 = array(
+      '1' => array(),
+      '2'=> array(),
+      '3'=> array(),
+      '4'=> array()
+  );
   // Instantiate DB & connect
   $database = new Database();
   $db = $database->connect();
@@ -34,9 +39,11 @@
       // Push to "data"
       #$posts_arr = []
       array_push($posts_arr, $post_item);
+      array_push($posts_arr2['1'], $post_item);
       // array_push($posts_arr['data'], $post_item);
     }
-    echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+
+    #echo json_encode($posts_arr2,JSON_UNESCAPED_SLASHES);
     $result = $post->read2();
     // Get row count
     $num = $result->rowCount();
@@ -57,10 +64,10 @@
 
         // Push to "data"
         #$posts_arr = []
-        array_push($posts_arr, $post_item2);
+        array_push($posts_arr2['2'], $post_item2);
         // array_push($posts_arr['data'], $post_item);
       }
-      echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+      #echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
       $result = $post->read3();
       // Get row count
       $num = $result->rowCount();
@@ -81,10 +88,10 @@
 
           // Push to "data"
           #$posts_arr = []
-          array_push($posts_arr, $post_item3);
+        array_push($posts_arr2['3'], $post_item3);
           // array_push($posts_arr['data'], $post_item);
         }
-        echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+        #echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
         $result = $post->read4();
         // Get row count
         $num = $result->rowCount();
@@ -105,10 +112,11 @@
 
             // Push to "data"
             #$posts_arr = []
-            array_push($posts_arr, $post_item4);
+
+        array_push($posts_arr2['4'], $post_item4);
             // array_push($posts_arr['data'], $post_item);
           }
-          echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+          echo json_encode($posts_arr2,JSON_UNESCAPED_SLASHES);
         }
       }
     }
