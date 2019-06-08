6e9ec3496ab01c3a4fdc43048947ee78a44c70de
Editscode <38367082+Editscode@users.noreply.github.com>
2019-06-08 15:29:49 +1000

    read 4 table

---
+++ API/api/post/read_4blocs.php
@@ -0,0 +1,124 @@
+<?php
+  // Headers
+  header('Access-Control-Allow-Origin: *');
+  header('Content-Type: application/json');
+
+  include_once '../../config/Database.php';
+  include_once '../../models/Post.php';
+
+  // Instantiate DB & connect
+  $database = new Database();
+  $db = $database->connect();
+  // Instantiate blog post object
+  $post = new Post($db);
+
+  // Blog post query
+  $result = $post->read1();
+  // Get row count
+  $num = $result->rowCount();
+
+  // Check if any posts
+  if($num > 0) {
+    // Post array
+    $posts_arr = array();
+    // $posts_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+      extract($row);
+
+      $post_item = array(
+        'id_ganre' => $id_ganre,
+        'name' => $name
+      );
+
+      // Push to "data"
+      #$posts_arr = []
+      array_push($posts_arr, $post_item);
+      // array_push($posts_arr['data'], $post_item);
+    }
+    echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+    $result = $post->read2();
+    // Get row count
+    $num = $result->rowCount();
+
+    // Check if any posts
+    if($num > 0) {
+      // Post array
+      $posts_arr = array();
+      // $posts_arr['data'] = array();
+
+      while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        extract($row);
+
+        $post_item2 = array(
+          'id_video_type' => $id_video_type,
+          'name' => $name
+        );
+
+        // Push to "data"
+        #$posts_arr = []
+        array_push($posts_arr, $post_item2);
+        // array_push($posts_arr['data'], $post_item);
+      }
+      echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+      $result = $post->read3();
+      // Get row count
+      $num = $result->rowCount();
+
+      // Check if any posts
+      if($num > 0) {
+        // Post array
+        $posts_arr = array();
+        // $posts_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+          extract($row);
+
+          $post_item3 = array(
+            'id_exit_status' => $id_exit_status,
+            'name' => $name
+          );
+
+          // Push to "data"
+          #$posts_arr = []
+          array_push($posts_arr, $post_item3);
+          // array_push($posts_arr['data'], $post_item);
+        }
+        echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+        $result = $post->read4();
+        // Get row count
+        $num = $result->rowCount();
+
+        // Check if any posts
+        if($num > 0) {
+          // Post array
+          $posts_arr = array();
+          // $posts_arr['data'] = array();
+
+          while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $post_item4 = array(
+              'age_limitations_table' => $age_limitations_table,
+              'name' => $name
+            );
+
+            // Push to "data"
+            #$posts_arr = []
+            array_push($posts_arr, $post_item4);
+            // array_push($posts_arr['data'], $post_item);
+          }
+          echo json_encode($posts_arr,JSON_UNESCAPED_SLASHES);
+        }
+      }
+    }
+  }
+
+
+
+
+    // Turn to JSON & output
+
+
+
+?>
--- API/models/Post.php
+++ API/models/Post.php
@@ -24,6 +24,53 @@
       return $stmt;
     }
 
+    public function read1() {
+      // Create query
+      $query1 = 'SELECT DISTINCT * FROM ganre_table';
+      // Prepare statement
+      $stmt1 = $this->conn->prepare($query1);
+      // Execute query
+
+      $stmt1->execute();
+
+      return $stmt1;
+
+    }
+    public function read2() {
+      // Create query
+      $query2 = 'SELECT DISTINCT * FROM anime_type_table';
+      // Prepare statement
+      $stmt2 = $this->conn->prepare($query2);
+      // Execute query
+
+      $stmt2->execute();
+
+      return $stmt2;
+
+    }
+    public function read3() {
+      // Create query
+      $query3 = 'SELECT DISTINCT * FROM exit_status_table';
+      // Prepare statement
+      $stmt3 = $this->conn->prepare($query3);
+      // Execute query
+      $stmt3->execute();
+
+      return $stmt3;
+
+    }
+    public function read4() {
+      // Create query
+      $query4 = 'SELECT DISTINCT * FROM age_limitations_table';
+      // Prepare statement
+      $stmt4 = $this->conn->prepare($query4);
+      // Execute query
+      $stmt4->execute();
+
+      return $stmt4;
+
+    }
+
     public function readt() {
       // Create query
       $query = 'SELECT * FROM ' . $this->table . '';
