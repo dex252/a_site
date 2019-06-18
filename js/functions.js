db29b5b416fb6b72460088ef508e6574ee77a353
Slava Solodkov <dex252@bk.ru>
2019-06-18 20:57:24 +1000

    Add discription, next=>chose series/get-requests

--- js/functions.js
+++ js/functions.js
@@ -60,7 +60,7 @@ function iSeeDeadPeople(e){
 		backIcon.id = "BackIcon";
 		backIcon.className = "BackIcon";
 		
-		//вставляем картинку картинку
+		//вставляем картинку
 		var img=document.createElement('div');
 		img.id = "img"+i;
 		img.className = "animeImage";
@@ -71,18 +71,48 @@ function iSeeDeadPeople(e){
 		discription.id = "txt"+i;
 		discription.className = "Discription";
 		
+		nameText = data[i-1]["name"]
+		exitStatusText = data[i-1]["id_exit_status"]
+	//	ganreText = data[i-1]["id_ganre"] //запрос по id жанра
+		yearText = data[i-1]["year"]
+		authorText = data[i-1]["author"]
+		videoTypeText = data[i-1]["id_video_type"]
+		numSeriesText = data[i-1]["num_series"]
+		discriptionText = data[i-1]["discription"]
+		
+		discription.innerHTML +='<p style="text-align:center;  font-weight: 600;">'+nameText+'</p>'
+		discription.innerHTML +='<p>Статус: '+exitStatusText+'</p>'
+		discription.innerHTML +='<p>Год: '+yearText+'</p>'
+		//discription.innerHTML +='<p>Жанр: '+ganreText+'</p>'
+		discription.innerHTML +='<p>Автор: '+authorText+'</p>'
+		discription.innerHTML +='<p>Тип: '+videoTypeText+'</p>'
+		discription.innerHTML +='<p>Серии: '+numSeriesText+'</p>'
+		discription.innerHTML +='<p>'+discriptionText+'</p>'
+		
+		var watchVideo = document.createElement('div');
+		watchVideo.id = "WatchVideo";
+		watchVideo.className = "watchChoseVideoText";
+		watchVideo.innerHTML +='<p style="text-align:center; font-size:20px; font-weight: 600;">Смотреть видео:</p>'
+		
 		var video = document.createElement('div');
 		//img_link = data[i-1]["img_link"] <-сюда вставить видюхи?
 		video.id = "video";
 		video.className = "Video";
-		video.innerHTML += '<iframe width="650" height="400" src="http://video.sibnet.ru/shell.php?videoid=3632282" frameborder="0" scrolling="no" allowfullscreen="" wmode="window" align="middle" style="position:relative; z-index:500;"></iframe>';	
+		video.innerHTML += '<iframe width="100%" height="400" src="http://video.sibnet.ru/shell.php?videoid=3632282" frameborder="0" scrolling="no" allowfullscreen="" wmode="window" align="middle" style="position:relative; z-index:500;"></iframe>';	
+		
+		var choseVideo = document.createElement('div');
+		choseVideo.id = "watchVideo";
+		choseVideo.className = "watchChoseVideoText";
+		choseVideo.innerHTML +='<p style="text-align:center; font-size:20px; font-weight: 600;">Выбрать серию:</p>'
 		
 		//добавляем созданные блоки в main
 		animeMainField.appendChild(back);
 		back.appendChild(backIcon);
 		animeMainField.appendChild(img);
 		animeMainField.appendChild(discription);
+		animeMainField.appendChild(watchVideo);
 		animeMainField.appendChild(video);
+		animeMainField.appendChild(choseVideo);
 		
 		//функция для возврата
 		$("back").click(function(){	
