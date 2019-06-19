d31c004800e5bb8fdfbe439fac5d40f0ad6e6eb6
Editscode <38367082+Editscode@users.noreply.github.com>
2019-06-19 15:56:45 +1000

    Update create_user.php

--- API/api/autification/create_user.php
+++ API/api/autification/create_user.php
@@ -3,3 +3,45 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../config/Database.php';
+include_once '../../models/user.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$user = new User($db);
+// submitted data will be here
+
+
+// get posted data
+$data = json_decode(file_get_contents("php://input"));
+
+// set product property values
+$user->firstname = $data->firstname;
+$user->lastname = $data->lastname;
+$user->email = $data->email;
+$user->password = $data->password;
+
+// use the create() method here
+if(
+    !empty($user->firstname) &&
+    !empty($user->email) &&
+    !empty($user->password) &&
+    $user->create()
+){
+
+    // set response code
+    http_response_code(200);
+    // display message: user was created
+    echo json_encode(array("message" => "User was created."));
+}
+
+// message if unable to create user
+else{
+
+    // set response code
+    http_response_code(400);
+    // display message: unable to create user
+    echo json_encode(array("message" => "Unable to create user."));
+}
